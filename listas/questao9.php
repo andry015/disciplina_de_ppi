50e5f8ddb4bfc20b4e28ea839d43f0b7f5c0ea3b
Andrielly Vitória da Costa Rodrigues  Serejo <20241134010014@ifrn.local>
2026-08-26 17:49:19 -0300

    ...

--- listas/questao9.php
+++ listas/questao9.php
@@ -1,15 +1,21 @@
 <?php
 
-$num = 1;
+$c = 0;
+$num = 18;
 
-while($num < 21){
-    if($num % 1 == 0 && $num % $num == 0){
-        echo $num . ' é primo' . "\n";
-    } elseif($num % 1 != 0 && $num % $num != 0){
-        echo $num . ' não é primo' . "\n";
-    }
+for($i = 1; $i <= $num; $i++){
+    if($num % 1 == 0 && $num % $i == 0){
+        $c++;
+    } 
 
-    $num++;
+}
+
+if($c == 2){
+    echo $num . ' é primo' . "\n";   
+}  
+
+else{
+    echo $num . ' não é primo' . "\n";
 }
 
 ?>
