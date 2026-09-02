23c78c5fdf8d8b3755aa92ffc4f4ade74e094915
Andrielly Vitória da Costa Rodrigues  Serejo <20241134010014@ifrn.local>
2026-09-02 14:49:38 -0300

    ...

--- listas/parte2/Q12.php
+++ listas/parte2/Q12.php
@@ -3,6 +3,7 @@
 $notas = [7.5, 4.0, 8.5, 6.0, 9.0, 3.5, 10.0, 5.5];
 $media = 0;
 $soma = 0;
+$total = 0;
 $maiorque = 0;
 $menorque = 0;
 $maior = $notas[0];
@@ -10,7 +11,16 @@ $menor = $notas[0];
 
 
 foreach($notas as $nota){
-    $media = $soma + $nota;
+    $soma += $nota;
+    $total++;
+
+    if($nota > $maior){
+        $maior = $nota;
+    }
+
+    if($nota < $menor){
+        $menor = $nota;
+    }
 
     if($nota >= 6){
         $maiorque++;
@@ -19,4 +29,11 @@ foreach($notas as $nota){
     }
 }
 
+$media = $soma / $total;
+
+echo $media . "\n";
+echo $maiorque . "\n";
+echo $menorque . "\n";
+echo $maior . "\n";
+echo $menor;
 ?>
--- listas/parte2/Q15.php
+++ listas/parte2/Q15.php
@@ -7,5 +7,32 @@ $alunos = [
     ["nome" => "João", "nota1" => 6.0, "nota2" => 5.0],
     ["nome" => "Pedro", "nota1" => 3.0, "nota2" => 7.0]
     ];
+$mediag = 0;
+$mediai = 0;
+$soma = 0;
+$total = 0;
+$maiorm = 0;
+$menorm = 0;
+
+
+/*foreach($alunos as $aluno){
+    $soma += $aluno;
+    $total++;
+
+    if($mediai > $maiorm){
+        $maiorm = $mediai;
+    }
+    if($mediai < $menorm){
+        $menorm = $mediai;
+    }
+
+    if($mediai >= 6){
+        echo "Aprovado";
+    } else{
+        echo "Reprovado";
+    }
+}
+
+$mediag = $soma / $total;*/
 
 ?>
