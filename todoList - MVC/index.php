2b1519c847e80add92e4b9a56dc34afe724eb7e0
Andrielly Vitória da Costa Rodrigues  Serejo <20241134010014@ifrn.local>
2026-09-18 15:53:23 -0300

    subindo o anexo da view, porém não deu para visualizar nesse pc

---
+++ todoList - MVC/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/app/controller/controller.php';
+
+$controller = new tarefaController();
+
+$action = $_GET['action'] ?? 'index';
+
+switch ($action){
+    case 'criar': 
+        $controller->criar();
+        break;
+    case 'excluir': 
+        $controller->excluir();
+        break;
+    case defaut: 
+        $controller->index();
+        break;
+}
+
+?>
